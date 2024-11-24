a475fbcf51fcb5067d4dafe150d2f7b3383f172f
hgbeleza <developer.beleza06@gmail.com>
2024-11-23 21:52:33 -0700

    feat: implementhing auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
     { path: 'auth/login', component: LoginComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent, canActivate: [authGuard] }
 ];
